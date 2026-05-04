176f65b61ceba6d2f9aa6b17cb89bffd153e3dc6
alifzaidan <zaidanalif22710@gmail.com>
2026-05-04 13:06:39 +0700

    update build

---
+++ public/build/assets/skeleton-CGPepoSC.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-DVbFtPTa.js";import{c as r}from"./utils-fS3vIg4G.js";function m({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:r("bg-primary/10 animate-pulse rounded-md",t),...e})}export{m as S};
---
+++ public/build/assets/use-motion-value-BdlyiC-9.js
@@ -0,0 +1,1 @@
+import{r as t}from"./app-DVbFtPTa.js";import{u as a,M as u,c as r}from"./proxy-4NydgEW3.js";function i(o){const e=a(()=>r(o)),{isStatic:s}=t.useContext(u);if(s){const[,n]=t.useState(o);t.useEffect(()=>e.on("change",n),[])}return e}export{i as u};
