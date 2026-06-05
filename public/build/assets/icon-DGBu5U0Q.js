fdf5ad1c0d00b165b150040fcdbbd13bb78bd9f7
thoriqkhoir <thoriqkhoir537@gmail.com>
2026-06-05 08:55:48 +0700

    update build

--- public/build/assets/icon-DGBu5U0Q.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-BMlfqhpc.js";import{c as t}from"./utils-DSR9vfb3.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
