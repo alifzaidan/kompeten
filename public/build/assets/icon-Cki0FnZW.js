1ed39b5a766ceb86523b58a3169f13f23151cc39
bagussyahrijal <bagussyahrijal123@gmail.com>
2026-06-07 16:45:30 +0700

    modify logo app header and build

--- public/build/assets/icon-Cki0FnZW.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-COJN7tgE.js";import{c as t}from"./utils-DIT8sN4q.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
--- resources/js/components/app-header.tsx
+++ resources/js/components/app-header.tsx
@@ -117,8 +117,8 @@ export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
             <div className="border-sidebar-border/80 bg-background fixed top-0 right-0 left-0 z-40 border-b shadow-xs">
                 <div className="mx-auto flex h-16 items-center px-4 md:max-w-7xl">
                     <Link href="/" prefetch className="flex items-center space-x-2">
-                        <img src="/assets/images/logo-primary.png" alt="Kompeten" className="block w-32 fill-current dark:hidden" />
-                        <img src="/assets/images/logo-secondary.png" alt="Kompeten" className="hidden w-32 fill-current dark:block" />
+                        <img src="/assets/images/logo-primary.png" alt="Kompeten" className="block w-10 fill-current dark:hidden" />
+                        <img src="/assets/images/logo-secondary.png" alt="Kompeten" className="hidden w-10 fill-current dark:block" />
                     </Link>
 
                     <div className="ml-6 hidden h-full items-center space-x-6 lg:flex">
