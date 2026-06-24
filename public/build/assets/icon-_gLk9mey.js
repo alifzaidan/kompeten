4462180d971200ae7f1ba8e382890ba7e9997ae5
thoriqkhoir <thoriqkhoir537@gmail.com>
2026-06-24 10:17:58 +0700

    update build

---
+++ public/build/assets/icon-_gLk9mey.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-DnK6npl3.js";import{c as t}from"./utils-DVojPo19.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
---
+++ public/build/assets/index-BSmIejKD.js
@@ -0,0 +1,1 @@
+import{g as r,a}from"./app-DnK6npl3.js";var t=a();const e=r(t);export{e as R,t as r};
