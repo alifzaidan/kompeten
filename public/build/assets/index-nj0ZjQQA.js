a82e3fee922be2f5e6c37878264b7c3dd1e750d7
alifzaidan <zaidanalif22710@gmail.com>
2026-07-04 11:33:36 +0700

    update build

--- public/build/assets/index-nj0ZjQQA.js
+++
@@ -1,1 +0,0 @@
-import{g as r,a}from"./app-C7UbgR4M.js";var t=a();const e=r(t);export{e as R,t as r};
--- public/build/assets/use-motion-value-4ZKSRTtq.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./app-C7UbgR4M.js";import{u as a,M as u,c as r}from"./proxy-1ZHiZis2.js";function i(o){const e=a(()=>r(o)),{isStatic:s}=t.useContext(u);if(s){const[,n]=t.useState(o);t.useEffect(()=>e.on("change",n),[])}return e}export{i as u};
