46daad29ab4ed6da474dbc62c52a35983158ddca
thoriqkhoir <thoriqkhoir537@gmail.com>
2026-08-06 11:43:52 +0700

    update build

--- public/build/assets/icon-DxFpBVnl.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-CvNlgF8F.js";import{c as t}from"./utils-C0n5rZO5.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
