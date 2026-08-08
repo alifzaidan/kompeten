2aab58c87b65b4227192cccdab67a28934434025
alifzaidan <zaidanalif22710@gmail.com>
2026-08-08 13:52:52 +0700

    update build

---
+++ public/build/assets/use-motion-value-Cgkh0jNy.js
@@ -0,0 +1,1 @@
+import{r as t}from"./app-CJZXkiMV.js";import{u as a,M as u,c as r}from"./proxy-4N5Y1F2z.js";function i(o){const e=a(()=>r(o)),{isStatic:s}=t.useContext(u);if(s){const[,n]=t.useState(o);t.useEffect(()=>e.on("change",n),[])}return e}export{i as u};
