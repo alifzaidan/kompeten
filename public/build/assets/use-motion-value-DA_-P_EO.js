94efa7bd4bd3941bc180c38e4f9598b58e77ceb9
alifzaidan <zaidanalif22710@gmail.com>
2026-08-13 11:58:01 +0700

    update build

---
+++ public/build/assets/use-motion-value-DA_-P_EO.js
@@ -0,0 +1,1 @@
+import{r as t}from"./app-CMUYK9qX.js";import{u as a,M as u,c as r}from"./proxy-Bi-mA9Zf.js";function i(o){const e=a(()=>r(o)),{isStatic:s}=t.useContext(u);if(s){const[,n]=t.useState(o);t.useEffect(()=>e.on("change",n),[])}return e}export{i as u};
